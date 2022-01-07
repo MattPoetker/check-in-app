5ff406168b024f58cc52679f088b92515312b1ba
MattPoetker <matt.pworks@gmail.com>
2022-01-06 21:12:49 -0600

    inital view configuration, added tailwind, basic home page template design

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
-    })
-  ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-import': {},
+    'postcss-flexbugs-fixes': {},
+  }
+  // plugins: [
+  //   tailwindcss: {},
+  //   autoprefixer: {},
+  //   require('postcss-preset-env')({
+  //     autoprefixer: {
+  //       flexbox: 'no-2009'
+  //     },
+  //     stage: 3
+  //   })
+  // ]
 }
