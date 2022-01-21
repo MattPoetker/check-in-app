16af83b086aebd92f9d20b3a2112429a58b05554
matthewdanepeavoy <matthew.musicplayonline@gmail.com>
2022-01-20 22:05:48 -0700

    Edit / Update event screen UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       gray: colors.gray,
       green: colors.green,
       amber: colors.amber,
+      red: colors.red,
       indigo: colors.indigo,
       yellow: colors.yellow,
     },
